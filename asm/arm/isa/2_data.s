a391c13ddb270c400fb0fefe245e8f96b7793d59
activeion <ji.zhonghua@qq.com>
2018-11-26 19:55:10 +0800

    improve isa/2_data.s

--- asm/arm/isa/2_data.s
+++ asm/arm/isa/2_data.s
@@ -1,6 +1,7 @@
 .global _start
 _start:
     /* 3.4 数据处理指令 */
+
     // 3.12 
     mov r1, r0
     //mov pc, r14 //r14==0，本指令将跳转到start_执行
@@ -23,6 +24,18 @@ _start:
     mov r0, #0
     mov r1, #0x80000004
     movs r0, r1, lsl #1 //观察cpsr的变化
+
+    // 3.16 
+    mrs r0, cpsr
+    bic r0, r0, #0x0F
+    msr cpsr, r0
+    mov r0, #-1
+    mov r1, #0x1
+    movs r0, r1, lsl #1 //观察cpsr的变化
+
+loop: mov r0, r0
+    ldr pc, loop
+    
 
 .end
 
