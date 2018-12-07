72c1bf2127a7962e56e2cb364b42e092628a833e
activeion <ji.zhonghua@qq.com>
2018-12-07 11:56:40 +0800

    update qemu script

--- vexpress-a9@qemu/qemu-sd-user.sh
+++ vexpress-a9@qemu/qemu-sd-user.sh
@@ -8,8 +8,8 @@ qemu-system-arm \
     -append "root=/dev/mmcblk0 rw console=ttyAMA0 \
     ip=dhcp loglevel=8" \
     -sd ./a9rootfs.ext3 \
-    -net nic \
-    -net user \
+    -net nic -net user \
 
+    # -net nic -net user是默认配置，不写这一句也可以
     # -net user是qemu进程为Arch主机新增的网卡，其ip由qemu自动配置 
     # -net nic是arm虚拟机内部网卡，其ip由append参数中的ip参数配置
